272ec6cf7343f951623e7156b45a0134dcb4dcf4
John <engrobert.ouko@gmail.com>
2022-09-09 12:42:07 -0700

    initial commit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /*
- * main - Prints all possible combination of two-digit numbers in ascending order
+ * main - Prints all possible combinations of two two-digit numbers,
+ * ranging from 0-99, separated by a comma followed by a space
  *
  * Return: Always 0 (Success)
  */
